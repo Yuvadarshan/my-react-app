8d6c47d7f3704080f7287fbcd046c4d7761489a0
YUVADARSHAN M <74342375+Yuvadarshan@users.noreply.github.com>
2025-09-10 13:35:05 +0530

    updated teacher dashboard

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,7 +5,7 @@ import { auth, db } from '../firebase-config';
 import { doc, getDoc, setDoc } from 'firebase/firestore';
 import LoadingSpinner from './LoadingSpinner';
 import '../styles/Login.css';
-import logo from '/Users/yuvadarshanmurugan/my-react-app/src/assets/logo.png'; // Import the logo image
+import logo from 'src/assets/logo.png'; // Import the logo image
 
 const Login = ({ showNotification }) => {
   const [email, setEmail] = useState('');
